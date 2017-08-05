95be507145da309ca5b92d3effcfc4d7985cf211
tan phan <tanphan@gmail.com>
2017-08-05 17:11:38 +0700

    fix_validate_question_answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ApplicationRecord
   PARAMS_ATTRIBUTES = [:id, image_attributes: Image::PARAMS_ATTRIBUTES]
 
+  validates :content, presence: true
+
   belongs_to :category
 
   has_many :question_answers, dependent: :destroy
@@ -10,8 +12,7 @@ class Question < ApplicationRecord
   delegate :picture, :picture_url, to: :image, prefix: false, allow_nil: true
 
   accepts_nested_attributes_for :image, allow_destroy: true
-  accepts_nested_attributes_for :question_answers, reject_if: ->a{a[:content].blank?},
-    allow_destroy: true
+  accepts_nested_attributes_for :question_answers, allow_destroy: true
 
   scope :random_questions, ->{order "RANDOM() LIMIT 10"}
 end
